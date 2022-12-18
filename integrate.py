d8408f97d7e72cda2d0f5ce624ca14fad06d6f48
LZH <homepig.lzh@qq.com>
2022-12-18 15:06:59 +0800

    添加了积分

---
+++ integrate.py
@@ -0,0 +1,33 @@
+from sympy.abc import *
+from sympy import *
+import logging
+import lib.cmdin
+sx = False
+_a, _b = -1, 1
+if len(lib.cmdin.rin) == 0:
+    lib.cmdin.set_patterns([str, object, object], [str])
+    if len(lib.cmdin.args) == 2:
+        sx = True
+else:
+    lib.cmdin.set_patterns([object, object], [])
+    if len(lib.cmdin.args) == 2:
+        sx = True
+if sx:
+    lib.cmdin.set_patterns([object, object])
+    _a, _b = lib.cmdin.args
+else:
+    lib.cmdin.set_patterns([])
+
+fs = lib.cmdin.inputs
+for _f in fs:
+    try:
+        fx = sympify(_f)
+        if sx:
+            fx = integrate(fx, (x, _a, _b))
+        else:
+            fx = integrate(fx, x)
+            pass
+    except Exception as _e:
+        logging.error(_e)
+    else:
+        print(fx)
